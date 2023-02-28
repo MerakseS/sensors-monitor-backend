ad53dd19e63f152c37b8fd78108ee89cb216a87d
Sergei Losev <the.sergei.losev@gmail.com>
2023-02-28 12:24:31 +0300

    #10 Split Controller interface and implementation

---
+++ src/main/java/com/merakses/sensorsmonitorbackend/controller/DefaultSensorController.java
@@ -0,0 +1,66 @@
+package com.merakses.sensorsmonitorbackend.controller;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.merakses.sensorsmonitorbackend.dto.SensorRequestDto;
+import com.merakses.sensorsmonitorbackend.dto.SensorResponseDto;
+import com.merakses.sensorsmonitorbackend.entity.Sensor;
+import com.merakses.sensorsmonitorbackend.mapper.SensorMapper;
+import com.merakses.sensorsmonitorbackend.service.SensorService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/sensors")
+public class DefaultSensorController implements SensorController {
+
+    private final SensorMapper sensorMapper;
+    private final SensorService sensorService;
+
+    @Override
+    public ResponseEntity<SensorResponseDto> create(SensorRequestDto sensorRequestDTO) {
+        Sensor sensor = sensorMapper.mapRequestDtoToSensor(sensorRequestDTO);
+        sensor = sensorService.create(sensor);
+        SensorResponseDto responseDto = sensorMapper.mapSensorToResponseDto(sensor);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @Override
+    public ResponseEntity<List<SensorResponseDto>> getAll() {
+        List<Sensor> sensorList = sensorService.getAll();
+        return ResponseEntity.ok(sensorMapper.mapSensorListToResponseDtoList(sensorList));
+    }
+
+    @Override
+    public ResponseEntity<Page<SensorResponseDto>> getPage(int pageNumber) {
+        Page<Sensor> sensorPage = sensorService.getPage(pageNumber);
+        return ResponseEntity.ok(sensorPage.map(sensorMapper::mapSensorToResponseDto));
+    }
+
+    @Override
+    public ResponseEntity<SensorResponseDto> get(long id) {
+        Sensor sensor = sensorService.get(id);
+        return ResponseEntity.ok(sensorMapper.mapSensorToResponseDto(sensor));
+    }
+
+    @Override
+    public ResponseEntity<SensorResponseDto> update(long id, SensorRequestDto sensorRequestDto) {
+        Sensor sensor = sensorMapper.mapRequestDtoToSensor(sensorRequestDto);
+        sensor = sensorService.update(id, sensor);
+        return ResponseEntity.ok(sensorMapper.mapSensorToResponseDto(sensor));
+    }
+
+    @Override
+    public ResponseEntity<Object> delete(long id) {
+        sensorService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
+
--- src/main/java/com/merakses/sensorsmonitorbackend/controller/SensorController.java
+++ src/main/java/com/merakses/sensorsmonitorbackend/controller/SensorController.java
@@ -3,7 +3,6 @@ package com.merakses.sensorsmonitorbackend.controller;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,66 +10,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.merakses.sensorsmonitorbackend.dto.SensorRequestDto;
 import com.merakses.sensorsmonitorbackend.dto.SensorResponseDto;
-import com.merakses.sensorsmonitorbackend.entity.Sensor;
-import com.merakses.sensorsmonitorbackend.mapper.SensorMapper;
-import com.merakses.sensorsmonitorbackend.service.SensorService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/sensors")
-public class SensorController {
-
-    private final SensorMapper sensorMapper;
-    private final SensorService sensorService;
+public interface SensorController {
 
     @PostMapping
-    public ResponseEntity<SensorResponseDto> create(@RequestBody @Valid SensorRequestDto sensorRequestDTO) {
-        Sensor sensor = sensorMapper.mapRequestDtoToSensor(sensorRequestDTO);
-        sensor = sensorService.create(sensor);
-        SensorResponseDto responseDto = sensorMapper.mapSensorToResponseDto(sensor);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
-    }
+    ResponseEntity<SensorResponseDto> create(@RequestBody @Valid SensorRequestDto sensorRequestDTO);
 
     @GetMapping
-    public ResponseEntity<List<SensorResponseDto>> getAll() {
-        List<Sensor> sensorList = sensorService.getAll();
-        return ResponseEntity.ok(sensorMapper.mapSensorListToResponseDtoList(sensorList));
-    }
+    ResponseEntity<List<SensorResponseDto>> getAll();
 
     @GetMapping("/page/{page}")
-    public ResponseEntity<Page<SensorResponseDto>> getPage(@PathVariable("page") @Min(0) int pageNumber) {
-        Page<Sensor> sensorPage = sensorService.getPage(pageNumber);
-        return ResponseEntity.ok(sensorPage.map(sensorMapper::mapSensorToResponseDto));
-    }
+    ResponseEntity<Page<SensorResponseDto>> getPage(@PathVariable("page") @Min(0) int pageNumber);
 
     @GetMapping("/{id}")
-    public ResponseEntity<SensorResponseDto> get(@PathVariable long id) {
-        Sensor sensor = sensorService.get(id);
-        return ResponseEntity.ok(sensorMapper.mapSensorToResponseDto(sensor));
-    }
+    ResponseEntity<SensorResponseDto> get(@PathVariable long id);
 
     @PutMapping("/{id}")
-    public ResponseEntity<SensorResponseDto> update(@PathVariable long id,
-        @RequestBody @Valid SensorRequestDto sensorRequestDto) {
-
-        Sensor sensor = sensorMapper.mapRequestDtoToSensor(sensorRequestDto);
-        sensor = sensorService.update(id, sensor);
-        return ResponseEntity.ok(sensorMapper.mapSensorToResponseDto(sensor));
-    }
+    ResponseEntity<SensorResponseDto> update(@PathVariable long id, @RequestBody @Valid SensorRequestDto sensorRequestDto);
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> delete(@PathVariable long id) {
-        sensorService.delete(id);
-        return ResponseEntity.ok().build();
-    }
+    ResponseEntity<Object> delete(@PathVariable long id);
 }
-
